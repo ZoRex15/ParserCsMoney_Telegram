afe5add5c0678581d1221c4b43156492b0dcecf6
reddmon <reddmonchick375@gmail.com>
2024-02-23 20:40:04 +0300

    delete threading rework parser add argument in function start min price and max_price add changes user-agent in headers

--- parser/main.py
+++ parser/main.py
@@ -3,55 +3,56 @@ import json
 from anti_useragent import UserAgent
 import time
 import concurrent.futures
-from data import data_, col_potokov
+from data import data_ as info, col_potokov
 from loguru import logger
 from service.rebbit import RebbitMQ
 from itertools import cycle
-#from database.requests import Database
+from database.requests import Database
 
 
 
-c = 0
 logger.add('debug.log', format="{time} {level} {message}", level='DEBUG', rotation='100 MB', compression='zip')
+ua = UserAgent()
 
-def start(tupl: tuple):
-    name, price = tupl[0], float(tupl[1])
-    global c
+def start(min_price: int = 0,max_price: int = 0):
 
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0',
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-        # 'Accept-Encoding': 'gzip, deflate, br',
-        'X-Client-App': 'web',
-        'traceparent': '00-c518993e8e67dd15a917fe97067735f7-c6fd71b0dd9858d5-01',
-        'Alt-Used': 'cs.money',
-        'Connection': 'keep-alive',
-        'Referer': 'https://cs.money/market/buy/',
-        # 'Cookie': 'region=Grodnenskaya; cc_service2={%22services%22:[%22necessary%22%2C%22gtm%22%2C%22ym%22%2C%22amplitude%22%2C%22gleam%22%2C%22esputnik%22%2C%22hotjar%22%2C%22userSesDatToAnalytic%22%2C%22userSesDatToMarketing%22%2C%22cardVisualSize%22]%2C%22acceptanceDate%22:1708281882726%2C%22revision%22:0}; group_id=d42c470d-7dea-4e74-add3-104d15440d4e; sc=5130948A-2EFB-54FC-AF9F-339C47DB13B2; _gcl_au=1.1.42814056.1708281883; _ga_HY7CCPCD7H=GS1.1.1708406151.3.1.1708406732.60.0.0; _ga=GA1.1.894398092.1708281884; cf_clearance=OjXPtZSxmcGvvvs8cWv8uYImO58e4vboOBKBS61wm8s-1708406149-1.0-AWKlDIMlvQn6DzCzb16wKSkLtbewwWeLGbhJI4Qa5VUoOrlrBGlq4aoP+Lp6CbhjhZLZ5bft4KOcj2uokBbXRL4=; _ga_CFRN8YJV66=GS1.1.1708406151.3.0.1708406151.0.0.0; ouid=60299031_1734003192; _ym_uid=1708281886412497733; _ym_d=1708281886; _scid=eafb7b8f-21ae-4a77-99ec-d91424da7f0a; amplitude_id_c14fa5162b6e034d1c3b12854f3a26f5cs.money=eyJkZXZpY2VJZCI6IjMwYzc3N2U0LTBmZGMtNDVhMi1hYzM5LTQ1ZDhjOTc3MWU5MVIiLCJ1c2VySWQiOm51bGwsIm9wdE91dCI6ZmFsc2UsInNlc3Npb25JZCI6MTcwODQwNjE0NzUwNywibGFzdEV2ZW50VGltZSI6MTcwODQwNjczMjYwMiwiZXZlbnRJZCI6MjksImlkZW50aWZ5SWQiOjM3LCJzZXF1ZW5jZU51bWJlciI6NjZ9; _hjSessionUser_2848248=eyJpZCI6IjU1N2EwYmJhLTAzYWItNWJlMy1hNTg2LTNhMTY4NjhiOWZhNyIsImNyZWF0ZWQiOjE3MDgyODE4ODY0ODAsImV4aXN0aW5nIjp0cnVlfQ==; _sctr=1%7C1708203600000; new_language=en; _hjHasCachedUserAttributes=true; _ym_isad=1; onboarding__skin_quick_view=false; isAnalyticEventsLimit=true; _hjSession_2848248=eyJpZCI6IjdlOWU1ODQ1LTZjYTQtNGFhNS05NTI1LWZkY2I5NmU5MDViNiIsImMiOjE3MDg0MDYxNDc3MzUsInMiOjEsInIiOjAsInNiIjowLCJzciI6MCwic2UiOjAsImZzIjowLCJzcCI6MX0=; _scid_r=eafb7b8f-21ae-4a77-99ec-d91424da7f0a; _uetsid=c962ac70ce8d11eeb5d901acd8280a1f; _uetvid=ae42a8205d4f11ee829917d87d31b452; _ym_visorc=b',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-origin',
+        'authority': 'cs.money',
+        'accept': 'application/json, text/plain, */*',
+        'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+        # 'cookie': 'region=Grodnenskaya; group_id=b62119c5-797f-47f9-8963-0208f5187fd0; cc_service2={%22services%22:[%22necessary%22%2C%22gtm%22%2C%22ym%22%2C%22amplitude%22%2C%22gleam%22%2C%22esputnik%22%2C%22hotjar%22%2C%22userSesDatToAnalytic%22%2C%22userSesDatToMarketing%22%2C%22cardVisualSize%22]%2C%22acceptanceDate%22:1708534353971%2C%22revision%22:0}; _gcl_au=1.1.996797232.1708534356; _hjSessionUser_2848248=eyJpZCI6IjhiMWMyM2MzLTc2M2MtNTA1Zi1hOGEyLWE2ZmU1YTNhMjczNiIsImNyZWF0ZWQiOjE3MDg1MzQzNTU5MTUsImV4aXN0aW5nIjp0cnVlfQ==; _ga=GA1.1.1085143897.1708534356; sc=F16E4144-E979-C8BD-CC14-75FAB188DB32; ouid=3272362712_3726229750; _ym_uid=1708534357629095503; _ym_d=1708534357; _scid=822a2d8f-f0db-4905-a830-d1f2f0de2057; _fbp=fb.1.1708534357459.944632933; _sctr=1%7C1708462800000; _ga_HY7CCPCD7H=deleted; cf_clearance=y4VJWtJt0uDF5N4sfYHww08xnLs9PcnpTGAIc2A2.QM-1708707736-1.0-AU43frJim5qxoqY/5q9iz9JLOjp2a4p6Ah26E1HNjvNeGv5dtmWEb+0k2eEW7BpI8YXHgt66aU35Xhzzhb3zUbg=; _gcl_aw=GCL.1708707739.Cj0KCQiAoeGuBhCBARIsAGfKY7xZ2otSJHClGIlOsJXK4VXemBeJYXeuX65kNVon0dQqnxv0UBoh0MgaApeXEALw_wcB; _hjSession_2848248=eyJpZCI6ImUxZWQ0OWFjLTBmNmMtNDIwMi1iZGIzLTJlZTVlZTViZTczOCIsImMiOjE3MDg3MDc3MzkyMDAsInMiOjEsInIiOjAsInNiIjowLCJzciI6MCwic2UiOjAsImZzIjowLCJzcCI6MX0=; _hjHasCachedUserAttributes=true; _ym_isad=2; _ym_visorc=b; isAnalyticEventsLimit=true; new_language=en; _scid_r=822a2d8f-f0db-4905-a830-d1f2f0de2057; _ga_HY7CCPCD7H=GS1.1.1708707739.2.1.1708707791.8.0.0; _ga_CFRN8YJV66=GS1.1.1708707739.2.1.1708707791.0.0.0; _uetsid=4de90a60d26d11eebc8e554ab0604254; _uetvid=9e1a3600d0d911ee9cd2979f53a72729; amplitude_id_c14fa5162b6e034d1c3b12854f3a26f5cs.money=eyJkZXZpY2VJZCI6IjBkZDU2YTE3LTczMmMtNDE1NC05YzRkLWZmNGFmOTU1NTYzMlIiLCJ1c2VySWQiOm51bGwsIm9wdE91dCI6ZmFsc2UsInNlc3Npb25JZCI6MTcwODcwNzczODcwNSwibGFzdEV2ZW50VGltZSI6MTcwODcwNzgwMjUzOCwiZXZlbnRJZCI6MjUsImlkZW50aWZ5SWQiOjIxLCJzZXF1ZW5jZU51bWJlciI6NDZ9',
+        'referer': 'https://cs.money/market/buy/',
+        'sec-ch-ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'traceparent': '00-56e16fe32fb63048cf5a68a520698048-f354c577f0c71e2e-01',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+        'x-client-app': 'web_mobile',
     }
-    offset = 0
-    batch_size = 60
     while True:
         try:
+            c = 0
+            col_skinov = 0
             params = {
                 'limit': '60',
-                'name': '',
-                'offset': '0',
-                'order': 'asc',
-                'sort': 'price',
+                'maxPrice': '',
+                'minPrice': '',
+                'order': 'desc',
+                'sort': 'discount',
             }
-            params['name'] = name
+            params['maxPrice'] = max_price
+            params['minPrice'] = min_price
             hash = []
             for i in range(0, 5000, 60):
                 params['offset'] = i
+                headers['user-agent'] = ua.random
                 #proxi = next(proxy)
                 #proxy_ye = dict(http=f'socks5://{proxi}',
                 #               https=f'socks5://{proxi}')
-                response = requests.get('https://cs.money/1.0/market/sell-orders', params=params, headers=headers)
+                response = requests.get('https://cs.money/1.0/market/sell-orders', params=params,headers=headers)
                 if response.status_code == 200:
                     c += 1
                     logger.info(f'Количество запросов {c}')
@@ -60,48 +61,21 @@ def start(tupl: tuple):
                         hash.append((item.get('pricing').get('computed'),
                                      item.get('asset').get("images").get('steam'),
                                      item.get('asset').get('names').get('full')))
-
-                    filterest_item = list(filter(lambda x: x[2] == name,hash))
-                    if len(filterest_item) > 0:
-                        lowest_item = min(filterest_item, key=lambda x: x[0])
-                        if lowest_item[0] <= price:
-                            logger.info(f'''Нашли скин с низкой ценой! {name}. Market price: {lowest_item[0]}, Json price: {price}''')
-                            RebbitMQ.send_message(
-                                photo_url=lowest_item[1],
-                                name=lowest_item[2],
-                                price=lowest_item[0],
-                                url=f'https://cs.money/market/buy/?limit=60&search={lowest_item[2]}&order=asc&sort=price')
-                            break
-                        else:
-                            break
-                    else:
-                        pass
-
                 else:
                     logger.warning(f'Статус код {response.status_code}')
                     break
-            break
+
+            for item_ in hash:
+                price,img,name = item_
+
+                price_json = info.get(name,999_999_999)
+                if float(price) <= float(price_json):
+                    col_skinov += 1
+                    RebbitMQ.send_message(
+                        photo_url=img,
+                        name=name,
+                        price=price,
+                        url=f'https://cs.money/market/buy/?limit=60&search={name}&order=asc&sort=price')
+            logger.debug(f'Количество скинов которые отправили {col_skinov}')
         except Exception as ex:
-
             logger.error(f'Ошибка в парсере {ex} Статус код: {response.status_code}')
-
-
-if __name__ == '__main__':
-    list_ = data_  # сюда вставляешь то что прогоняешь через фор смотри где больше ссылок собрано
-    CONNECTIONS = col_potokov  # колличетсво потоков
-    out = []
-    #with open('proxy.txt', 'r', encoding='utf-8') as file:
-        #proxyy_ = list(map(lambda x: x.strip(), file.readlines()[2]))
-    #proxy = cycle(proxyy_)
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=CONNECTIONS) as executor:
-        future_to_url = {executor.submit(start, tupl): tupl for tupl in list_}
-        done, _ = concurrent.futures.wait(future_to_url, return_when=concurrent.futures.ALL_COMPLETED)
-        for future in done:
-            try:
-                data = future.result()
-            except Exception as exc:
-                data = str(type(exc))
-            finally:
-                print('многопоток завершается')
-                out.append(data)
