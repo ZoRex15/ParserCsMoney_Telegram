95e4e9de899a6b5060f3696d557bb561057064e5
reddmonchick <116973364+reddmonchick@users.noreply.github.com>
2024-02-24 23:55:31 +0300

    Update parser_1.py
    debug

--- parser_1.py
+++ parser_1.py
@@ -65,7 +65,7 @@ def start(min_price: int | float = 0,max_price: int | float = 0):
                 price,img,name = item_
                 price_json = info.get(name,999_999_999)
                 if float(price) <= float(price_json):
-                    logger.debug(f'''Нашли скин с низкой ценой! {name}. Market price: {lowest_item[0]}, Json price: {price}''')
+                    logger.debug(f'''Нашли скин с низкой ценой! {name}. Market price: {price_json}, Json price: {price}''')
                     col_skinov += 1
                     RebbitMQ.send_message(
                         photo_url=img,
